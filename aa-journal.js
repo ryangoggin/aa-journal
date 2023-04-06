f54ebe328cbf193110a61b4bbdb9511060b0d8c0
Ryan Goggin <ryangoggin1@gmail.com>
2023-04-05 20:27:13 -0400

    added new repo links and week/day titles, still need day details up to week 23

--- aa-journal.js
+++ aa-journal.js
@@ -14,17 +14,35 @@ window.onload = () => {
     githubBtn.addEventListener("click", goToGithub);
 
     //make project containers links to their GitHub repos
+    //Backend Airbnb Project:
     const backendProj = document.querySelector(".backend_project");
 
     const goToBackendRepo = () => {
-        window.open("https://github.com/ryangoggin/AirBnB-clone-project", "_blank");
+        window.open("https://github.com/ryangoggin/GoggInn", "_blank");
     }
     backendProj.addEventListener("click", goToBackendRepo);
 
+    // Frontend Airbnb Project:
     const frontendProj = document.querySelector(".frontend_project");
 
     const goToFrontendRepo = () => {
-        window.open("https://github.com/ryangoggin/AirBnB-clone-project", "_blank");
+        window.open("https://github.com/ryangoggin/GoggInn", "_blank");
     }
     frontendProj.addEventListener("click", goToFrontendRepo);
+
+    // Group Project:
+    const groupProj = document.querySelector(".group_project");
+
+    const goToGroupRepo = () => {
+        window.open("https://github.com/ryangoggin/PixelPal", "_blank");
+    }
+    groupProj.addEventListener("click", goToGroupRepo);
+
+    // Capstone Project:
+    const capstoneProj = document.querySelector(".capstone_project");
+
+    const goToCapstoneRepo = () => {
+        window.open("https://github.com/ryangoggin/Goggbook", "_blank");
+    }
+    capstoneProj.addEventListener("click", goToCapstoneRepo);
 }
